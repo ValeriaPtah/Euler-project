2a42490e1e71af6546241259650595cf54392e02
Valerie <it2ava@gmail.com>
2016-06-27 23:46:30 +0300

    first attempt (errors)
    testSum check failed, working on it

--- src/problem1/Multiples.java
+++ src/problem1/Multiples.java
@@ -1,15 +1,45 @@
 package problem1;
+
 public class Multiples {
 	
-	private int div1 = 3;
-	private int div2 = 5;
+	private int div1;
+	private int div2;
 
-	//finds a sum of all numbers divided by div1 and/or div2
-	public int multipleSum (int n) {
+	public Multiples() {
+		this(3, 5);
+	}
+	
+	public Multiples(int div1, int div2) {
+		this.div1 = div1;
+		this.div2 = div2;
+	}
+	
+	//finds a sum of all numbers in n divided by div
+	public int arSum (int n, int div) {
 		int sum = 0; //returned sum
-		int firstMem = Math.min(div1, div2);
+		int firstMem = div;
+		int amount = n/div; //number of members
 		
+		sum = (2*firstMem + div*(amount-1))*((amount/2)+(amount%2));
 		
+		return sum;
+	}
+	
+	public int multSum (int div1, int div2, int n) {
+		int sum = 0;
+		sum = arSum(n, div1) + arSum(n, div2) - arSum(n, div1*div2);
+		//add two arithmetic progression with d=div1 and d=div2
+		//but extract sum of div1*div2 to avoid duplicates (15 for 3 and 5)
+		return sum;
+	}
+	
+	public int testSum (int div1, int div2, int n) {
+		int sum = 0;
+		for (int i=0; i<=n; i++) {
+			if ((i%div1==0) || (i%div1==0))
+				sum+=i;
+		}
+		//brute-force sum
 		return sum;
 	}
 }
--- src/problem1/Problem1.java
+++ src/problem1/Problem1.java
@@ -1,4 +1,5 @@
 package problem1;
+
 public class Problem1 {
 
 //	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
@@ -6,7 +7,9 @@ public class Problem1 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Multiples euler1 = new Multiples();
+		System.out.println(euler1.multSum(3, 5, 1000));
+		System.out.println(euler1.testSum(3, 5, 1000));
 	}
 
 }
