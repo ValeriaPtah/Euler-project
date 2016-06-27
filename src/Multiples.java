8e5107bb4e0d36fdfe3d3182f3346c891c455963
Valerie <it2ava@gmail.com>
2016-06-27 23:02:44 +0300

    Initialisation

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+	
+	private int div1 = 3;
+	private int div2 = 5;
+
+	//finds a sum of all numbers divided by div1 and/or div2
+	public int multipleSum (int n) {
+		int sum = 0; //returned sum
+		int firstMem = Math.min(div1, div2);
+		
+		
+		return sum;
+	}
+}
