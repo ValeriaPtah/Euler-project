b2525dcd7824afc27e093293b8773eba5f3bae4d
Valerie <it2ava@gmail.com>
2016-07-14 22:49:00 +0300

    pr5 brute force works

--- src/problem4/Problem4.java
+++ src/problem4/Problem4.java
@@ -8,6 +8,6 @@ public class Problem4 {
 
         PalindromeTriple euler4 = new PalindromeTriple();
         System.out.println(euler4.toString()); //997799, 580085, 698896 is wrong //906609 correct
-        
+
     }
 }
---
+++ src/problem5/Problem5.java
@@ -0,0 +1,18 @@
+package problem5;
+
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+
+        SmallestMultiple euler5 = new SmallestMultiple();
+        int n = 20;
+
+        System.out.println(euler5.toString(n));
+
+
+    }
+}
